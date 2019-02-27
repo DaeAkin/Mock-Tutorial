a6f26c175670aef57d214e867f8d6f34a3a56190
DonghyeonMin <kei890@naver.com>
2019-02-27 23:30:54 +0900

    수정

--- Mock-Tutorial/src/main/java/com/mock/tutorial/dao/MemberDao.java
+++ Mock-Tutorial/src/main/java/com/mock/tutorial/dao/MemberDao.java
@@ -3,7 +3,7 @@ package com.mock.tutorial.dao;
 import com.mock.tutorial.dto.MemberDto;
 
 public interface MemberDao {
-	
-	void insertMember(MemberDto member);
+
+	int insertMember(MemberDto member);
 
 }
