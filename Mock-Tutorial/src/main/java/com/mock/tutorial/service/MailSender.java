811a181e798e0364145183b76bc886aef916e292
DonghyeonMin <kei890@naver.com>
2019-02-27 13:27:53 +0900

    Setup Commit

---
+++ Mock-Tutorial/src/main/java/com/mock/tutorial/service/MailSender.java
@@ -0,0 +1,7 @@
+package com.mock.tutorial.service;
+
+public interface MailSender {
+
+	
+	void sendMail(String user_id);
+}
---
+++ Mock-Tutorial/src/main/java/com/mock/tutorial/test/MailSenderTest.java
@@ -0,0 +1,5 @@
+package com.mock.tutorial.test;
+
+public class MailSenderTest {
+
+}
